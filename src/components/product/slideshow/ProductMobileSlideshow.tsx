352097ee22384d349c9bb28983830f5d0b04effe
whoysAndre <yellowcata2121@gmail.com>
2024-02-01 14:45:56 -0500

    Actualizado

--- src/components/product/slideshow/ProductMobileSlideshow.tsx
+++ src/components/product/slideshow/ProductMobileSlideshow.tsx
@@ -10,6 +10,7 @@ import 'swiper/css/pagination';
 
 
 import './slideshow.css';
+import { ProductImage } from "../product-image/ProductImage";
 
 
 interface Props {
@@ -37,10 +38,10 @@ export const ProductMobileSlideshow = ({ images, title, className }: Props) => {
         {
           images.map(image => (
             <SwiperSlide key={image}>
-              <Image
+              <ProductImage
                 width={600}
                 height={500}
-                src={`/products/${image}`}
+                src={image}
                 alt={title}
                 className="object-fill"
               />
